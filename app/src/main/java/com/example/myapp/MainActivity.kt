4ddd464659a318af662355b456d2efa3863bbbc6
dovlatyanc <dovlatyanc.ru@gmail.com>
2026-02-26 20:54:49 +0300

    исправлены замечания

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapp
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,74 +9,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.example.myapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
 
     private var onCreateTime: Long = 0
-    private var onStartTimestamp: Long = 0
-    private var onResumeTimestamp: Long = 0
-    private var onPauseTimestamp: Long = 0
-    private var onStopTimestamp: Long = 0
-    private var onDestroyTimestamp: Long = 0
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         onCreateTime = System.currentTimeMillis()
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.openCalc).setOnClickListener {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openCalc.setOnClickListener {
             val intent = Intent(this, CalculatorActivity::class.java)
             startActivity(intent)
         }
 
-        Log.d(TAG, "onCreate: start = $onCreateTime")
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-
-    override fun onStart() {
-        onStartTimestamp = System.currentTimeMillis()
-        val durationOnCreateToOnStart = onStartTimestamp - onCreateTime
-        super.onStart()
-        Log.d(TAG, "onStart :  разница после onCreate = ${durationOnCreateToOnStart} мс")
-    }
-
-    override fun onResume() {
-        onResumeTimestamp = System.currentTimeMillis()
-
-        val durationOnStartToOnResume = onResumeTimestamp - onStartTimestamp
-        super.onResume()
-        Log.d(TAG, "onResume: разница после onStart = $durationOnStartToOnResume мс ")
-    }
-
-    override fun onPause() {
-        onPauseTimestamp = System.currentTimeMillis()
-        val duration = onPauseTimestamp - onResumeTimestamp
-        super.onPause()
-        Log.d(TAG, "onPause: +${duration} мс после onResume")
-    }
-
-    override fun onStop() {
-        onStopTimestamp = System.currentTimeMillis()
-        val duration = onStopTimestamp - onPauseTimestamp
-        super.onStop()
-        Log.d(TAG, "onStop: +${duration} мс после onPause")
-    }
-
-    override fun onDestroy() {
-        onDestroyTimestamp = System.currentTimeMillis()
-        val duration = onDestroyTimestamp - onStopTimestamp
-        super.onDestroy()
-        Log.d(TAG, "onDestroy: +${duration} мс после onStop")
 
     }
+
 }
