84388b2937d82e7ff179ca3e8ecd14e6b1ff4ae5
dovlatyanc <dovlatyanc.ru@gmail.com>
2026-03-01 17:35:54 +0300

    added CarListActivity

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,38 +1,30 @@
 package com.example.myapp
 
-
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.myapp.databinding.ActivityMainBinding
-class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val TAG = "MainActivity"
-    }
 
-    private var onCreateTime: Long = 0
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        onCreateTime = System.currentTimeMillis()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         binding.openCalc.setOnClickListener {
             val intent = Intent(this, CalculatorActivity::class.java)
             startActivity(intent)
         }
 
-
+        binding.btnCarsList.setOnClickListener {
+            val intent = Intent(this, CarsListActivity::class.java)
+            startActivity(intent)
+        }
     }
-
 }
---
+++ app/src/main/java/com/example/myapp/model/Car.kt
@@ -0,0 +1,10 @@
+package com.example.myapp.model
+
+data class Car(
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val description: String,
+    val cost: Int,
+    val imageResId: Int
+)
